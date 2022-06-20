235d7b244b8713a9820ec2fde8a01904620f9766
Kateper91 <99844046+Kateper91@users.noreply.github.com>
2022-06-20 20:13:42 -0300

    Add files via upload

---
+++ KarenTeper/forwhile.js
@@ -0,0 +1,5 @@
+for (let i = 1; i <=20; i++) {
+    let ingresarNombre = prompt ("Ingrese nombre del paciente")
+    let ingresarApellido = prompt ("Ingresa apellido del paciente ")
+       alert( " Paciente:  " + ingresarNombre +" "+ ingresarApellido + " Su turno es: " + i);
+    }
